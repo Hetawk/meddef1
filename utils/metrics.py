Fix ROC AUC and Averge Precision, added GAN and setup the generator and descriminator. Fix dimension problem on the adversarial example generation

utils/metrics.py
@@ -1,11 +1,10 @@
-# metrics.py
-
 import numpy as np
 from sklearn.metrics import (
     accuracy_score, precision_score, recall_score,
     f1_score, confusion_matrix, balanced_accuracy_score,
     matthews_corrcoef, roc_auc_score, average_precision_score
 )
+from sklearn.preprocessing import label_binarize
 from typing import Dict, Any, Optional
 
 class Metrics:
@@ -36,10 +35,12 @@ class Metrics:
         metrics['balanced_accuracy'] = balanced_accuracy_score(true_labels, all_predictions)
         metrics['mcc'] = matthews_corrcoef(true_labels, all_predictions)
 
-        # Ensure the correct shape for ROC-AUC calculation
-        if all_probabilities is not None and len(np.unique(true_labels)) == 2:
-            metrics['roc_auc'] = roc_auc_score(true_labels, all_probabilities)
-            metrics['average_precision'] = average_precision_score(true_labels, all_probabilities)
+        # Ensure the correct shape for ROC-AUC and average precision calculation
+        if all_probabilities is not None:
+            # Binarize the true labels for multi-class classification
+            true_labels_binarized = label_binarize(true_labels, classes=np.unique(true_labels))
+            metrics['roc_auc'] = roc_auc_score(true_labels_binarized, all_probabilities, average='macro', multi_class='ovr')
+            metrics['average_precision'] = average_precision_score(true_labels_binarized, all_probabilities, average='macro')
         else:
             metrics['roc_auc'] = None
             metrics['average_precision'] = None

utils/task_handler.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 import numpy as np
 import torch
+
+from gan.adversarial_example_generator import AdversarialExampleGenerator
 from gan.attack.attack_loader import AttackLoader
 from gan.attack.attacker import AttackHandler
 from gan.defense.defense_loader import DefenseLoader
@@ -42,6 +44,10 @@ class TaskHandler:
         self.trained_models = {}
         self.num_classes = num_classes
 
+        # Initialize AdversarialExampleGenerator
+        self.adversarial_example_generator = AdversarialExampleGenerator(noise_dim=100,
+                                                                         data_dim=input_channels_dict[dataset_name],
+                                                                         device=device)
     def run_train(self):
         """Runs the training process for the specified dataset and models."""
         self.current_task = 'normal_training'
@@ -108,6 +114,19 @@ class TaskHandler:
                     all_results.append(evaluator)
                 except Exception as e:
                     logging.error(f"Error evaluating model {model_name_with_depth}: {e}")
+
+                # Generate and visualize adversarial examples
+                original_data = []
+                for data, _ in test_loader:
+                    original_data.append(data)
+                original_data = torch.cat(original_data, dim=0)
+
+                adversarial_examples = self.adversarial_example_generator.generate_adversarial_examples(
+                    len(original_data))
+                self.adversarial_example_generator.visualize_adversarial_examples(
+                    original_data, adversarial_examples, model_name_with_depth, self.current_task,
+                    dataset_name, 'gan_attack'
+                )
 
         logging.info(f"Taskhandler: - Class names: {self.classes[dataset_name]}")
         self.visualization.visualize_normal(
@@ -256,7 +275,8 @@ class TaskHandler:
                 momentum=hyperparams.get('momentum', 0)
             )
 
-            if self.current_task in ['defense', 'attack']:
+            # if self.current_task in ['defense', 'attack']:
+            if self.current_task in ['normal_training']:
                 adversarial = True
 
             if self.current_task is None or dataset_name is None: